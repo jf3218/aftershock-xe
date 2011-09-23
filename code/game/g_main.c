54d0ddeac6b7fe6cdf80b01027ae0c44698b1e7a
oaUnofficial <oaUnofficial@c4d9a307-8deb-2bf4-7d76-ab10c62ac5ec>
2011-09-23 00:09:48 +0000

    Chg: g_forcerespawn cheatprotected and set to 5, renamed cg_lightningExplosion to cg_lightningImpact, cg_oldRocket/Rail/Plasma to cg_rocket/rail/plasmaStyle

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -307,7 +307,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_quadfactor, "g_quadfactor", "3", 0, 0, qtrue  },
 	{ &g_weaponRespawn, "g_weaponrespawn", "5", 0, 0, qtrue  },
 	{ &g_weaponTeamRespawn, "g_weaponTeamRespawn", "30", 0, 0, qtrue },
-	{ &g_forcerespawn, "g_forcerespawn", "20", 0, 0, qtrue },
+	{ &g_forcerespawn, "g_forcerespawn", "5", CVAR_CHEAT, 0, qtrue },
 	{ &g_inactivity, "g_inactivity", "0", 0, 0, qtrue },
 	{ &g_debugMove, "g_debugMove", "0", 0, 0, qfalse },
 	{ &g_debugDamage, "g_debugDamage", "0", 0, 0, qfalse },
