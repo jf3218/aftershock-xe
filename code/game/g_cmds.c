02473127c19f905efebadf5bad222eda65ea73e1
Olaf Lüke <olaf@tinkerforge.com>
2018-07-15 23:29:40 +0200

    Make sure flags can't disappear during a timeout
    Since flags are saved as powerups, the time was also incremented.
    
    But the flag "powerups" are set to INT_MAX, if we increase them
    they will overflow and the flag might disappear in the game...
    
    See Issue #8

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -1161,7 +1161,12 @@ void Cmd_Timeout_f( gentity_t *player ) {
 		for(i = 0; i < level.numConnectedClients; i++) {
 			for(j = 0; j < MAX_POWERUPS; j++) {
 				if(level.clients[i].ps.powerups[j] > 0) {
-					level.clients->ps.powerups[j] += level.timeoutAdd;
+					// The flag "powerups" are set to INT_MAX, if we increase them
+					// they will overflow and might disappear in the game...
+					// So we ignore them here
+					if((j != PW_REDFLAG) && (j != PW_BLUEFLAG) && (j != PW_NEUTRALFLAG)) {
+						level.clients->ps.powerups[j] += level.timeoutAdd;
+					}
 				}
 			}
 		}
