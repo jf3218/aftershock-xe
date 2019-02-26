49aaa39030be2e6b9e16635bfe8d7fc5102fd938
dedavidd <davidd@stack.nl>
2019-02-27 00:21:43 +0100

    announce time to server at completion

--- code/game/g_minigames.c
+++ code/game/g_minigames.c
@@ -133,7 +133,7 @@ void Touch_MinigameWaypoint (gentity_t *waypoint, gentity_t *ent, trace_t *trace
 			msecs -= secs*1000;
 			mins = secs/60;
 			secs -= mins*60;
-    trap_SendServerCommand(clientNum, va ("secho \"Found all %i waypoints in %i:%02i:%03i, reseting\"", numwaypoints,mins,secs,msecs));
+    trap_SendServerCommand(-1, va ("secho \"%s^7 Found all %i waypoints in %i:%02i:%03i, reseting\"", ent->client->pers.netname, numwaypoints,mins,secs,msecs));
     touchedWaypoints[clientNum] = mask;
     numTouchedWaypoints[clientNum]++;
     startTouchedWaypointTime[clientNum] = level.time;
