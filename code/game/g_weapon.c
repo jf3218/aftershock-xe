bfd52eb0f3c264a99abe5502d58a7623dffebbe6
spacebook <josec.rodriguez@gmail.com>
2021-03-20 14:32:09 +0100

    Backport from OA:  Backport from ioq3@51e9aa2: "Fix hit accuracy stats for lightning gun and shotgun kills"

--- code/game/g_weapon.c
+++ code/game/g_weapon.c
@@ -404,6 +404,7 @@ qboolean ShotgunPellet( vec3_t start, vec3_t end, gentity_t *ent ) {
 	gentity_t	*traceEnt;
 	vec3_t		impactpoint, bouncedir;
 	vec3_t		tr_start, tr_end;
+	qboolean    hitClient = qfalse;
 
 	passent = ent->s.number;
 	oldAttacker = ent;
@@ -438,13 +439,11 @@ qboolean ShotgunPellet( vec3_t start, vec3_t end, gentity_t *ent ) {
 				}
 				continue;
 			}
-			else {
-				G_Damage( traceEnt, ent, ent, forward, tr.endpos,
-					damage, 0, MOD_SHOTGUN);
-				if( LogAccuracyHit( traceEnt, ent ) ) {
-					return qtrue;
-				}
+			if ( LogAccuracyHit( traceEnt, ent) ) {
+				hitClient = qtrue;
 			}
+			G_Damage( traceEnt, ent, ent, forward, tr.endpos, damage, 0, MOD_SHOTGUN);
+			return hitClient;
 		}
 		return qfalse;
 	}
@@ -943,10 +942,13 @@ void Weapon_LightningFire( gentity_t *ent ) {
 				}
 				continue;
 			}
-			else {
-				G_Damage( traceEnt, ent, ent, forward, tr.endpos,
-					damage, 0, MOD_LIGHTNING);
+			if( LogAccuracyHit( traceEnt, ent ) ) {
+				if( level.time - traceEnt->client->lastActive < 500 )
+					G_AddHitHistory( ent->client, qtrue );		  
+				ent->client->accuracy_hits++;
+				ent->client->accuracy[WP_LIGHTNING][1]++;
 			}
+			G_Damage( traceEnt, ent, ent, forward, tr.endpos, damage, 0, MOD_LIGHTNING);
 		}
 
 		if ( traceEnt->takedamage && traceEnt->client ) {
@@ -954,14 +956,6 @@ void Weapon_LightningFire( gentity_t *ent ) {
 			tent->s.otherEntityNum = traceEnt->s.number;
 			tent->s.eventParm = DirToByte( tr.plane.normal );
 			tent->s.weapon = ent->s.weapon;
-			
-			if( LogAccuracyHit( traceEnt, ent ) ) {
-				if( level.time - traceEnt->client->lastActive < 500 )
-					G_AddHitHistory( ent->client, qtrue );
-			  
-				ent->client->accuracy_hits++;
-				ent->client->accuracy[WP_LIGHTNING][1]++;
-			}
 		} else if ( !( tr.surfaceFlags & SURF_NOIMPACT ) ) {
 			tent = G_TempEntity( tr.endpos, EV_MISSILE_MISS );
 			tent->s.eventParm = DirToByte( tr.plane.normal );
